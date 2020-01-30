1a6f3da18e1ea8839f9da9694478ca8b2c4177a0
Ewa Czech <ewa.czech@hotmail.com>
2020-01-30 14:20:14 +0100

    add timer function and change quarter based timer

--- src/App.js
+++ src/App.js
@@ -12,32 +12,47 @@ function App() {
 	const [valueLions, setValueLions] = useState(initialValueLions);
 	const [valueTigers, setValueTigers] = useState(initialValueTigers);
 
-	let [count, setCount] = useState(0);
+	// TIMER FUNCTION
+
+	let [seconds, setTimer] = useState(0);
+
+	const formatTime = seconds => {
+		let date = new Date(seconds * 1000);
+
+		let mm = date.getMinutes();
+		let ss = date.getSeconds();
+
+		if (mm < 10) {
+			mm = '0' + mm;
+		}
+		if (ss < 10) {
+			ss = '0' + ss;
+		}
+		return mm + ':' + ss;
+	};
 
 	useEffect(() => {
 		let timer;
-
-		if (count > 58) {
-			clearInterval(timer);
-		} else {
-			timer = setInterval(() => {
-				setCount(count + 1);
-			}, 1000);
+		if (seconds > 3599) {
+			return () => clearInterval(timer);
 		}
+		timer = setInterval(() => {
+			setTimer(seconds + 1);
+		}, 1000);
 		return () => clearInterval(timer);
 	});
 
 	let [quarter, setQuarter] = useState(1);
 
 	useEffect(() => {
-		count < 15
+		seconds < 900
 			? setQuarter(1)
-			: count < 30
+			: seconds < 1800
 			? setQuarter(2)
-			: count < 45
+			: seconds < 2700
 			? setQuarter(3)
 			: setQuarter(4);
-	}, [count]);
+	}, [seconds]);
 
 	const scoreHandler = (team, amount) => {
 		team === 'home'
@@ -56,7 +71,7 @@ function App() {
 
 						<div className="home__score">{valueLions}</div>
 					</div>
-					<div className="timer">{`00 :${count}`}</div>
+					<div className="timer">{formatTime(seconds)}</div>
 					<div className="away">
 						<h2 className="away__name">Tigers</h2>
 						<div className="away__score">{valueTigers}</div>
